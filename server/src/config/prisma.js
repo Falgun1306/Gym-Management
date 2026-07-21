675c9c94502fbc75dca5b99afd73b9e460850fb3
Falgun1306 <panchalfalgun3@gmail.com>
2026-07-21 11:27:41 +0530

    extra space remove

--- server/src/config/prisma.js
+++ server/src/config/prisma.js
@@ -5,4 +5,4 @@ import { PrismaClient } from "@prisma/client";
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
-export default prisma;
+export default prisma;
